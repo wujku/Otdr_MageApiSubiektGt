cd2bb76b85e9c837789930cfed9798afc8b11607
Luke <>
2018-08-17 14:54:36 +0200

    Wpięcie komunikacji JSON-owej dla api-subiekt-gt

--- Otdr/MageApiSubiektGt/Cron/OrderSend.php
+++ Otdr/MageApiSubiektGt/Cron/OrderSend.php
@@ -1,24 +1,37 @@
 <?php
 namespace Otdr\MageApiSubiektGt\Cron;
 
+use Otdr\MageApiSubiektGt\Helper\SubiektApi;
 
 class OrderSend 
 {
    protected $logger;
    protected $config;
    protected $ordersProcessed = 0;
+   protected $end_point = '';
+   protected $api_key = '';
+   protected $orderRepository = false;
 
 
-   public function __construct(\Otdr\MageApiSubiektGt\Helper\Config $config){
+   public function __construct(\Otdr\MageApiSubiektGt\Helper\Config $config,     \Magento\Sales\Model\OrderRepository $orderRepository){
       $this->config = $config;      
+      $this->orderRepository = $orderRepository;
+      $this->end_point = $this->config->getGen('subiekt_api_gateway');
+      $this->api_key = $this->config->getGen('subiekt_api_key');
    }
 
 
    public function execute()
    {
-      $this->config->getGen('subiekt_api_gateway');
-
-
+      
+      
+      $subiektApi = new SubiektApi($this->api_key,$this->end_point);
+      $result = $subiektApi->call('document/get',array("doc_ref"=>"PA 14588/08/2018"));            
+      //var_dump($result);
+      if(!$result){
+        var_dump($subiektApi->getPlainTextResult());
+      }
+      
       return true;
    }
 
---
+++ Otdr/MageApiSubiektGt/Helper/SubiektApi.php
@@ -0,0 +1,69 @@
+<?php
+namespace Otdr\MageApiSubiektGt\Helper;
+
+class SubiektApi
+{
+    private $api_key;
+    private $api_endpoint = '';
+    private $verify_ssl   = false;
+    private $result = false;
+
+    /**
+     * Create a new instance
+     */
+    function __construct($api_key,$api_endpoint){        
+        $this->api_key = $api_key;       
+        $this->api_endpoint = $api_endpoint;
+    }
+
+    /**
+     * Call request to api  
+     */
+    public function call($method, $args=array(), $timeout = 60){
+        return $this->makeRequest($method, $args, $timeout);
+    }
+
+    /**
+     * Performs the underlying HTTP request. Not very exciting
+     */
+    private function makeRequest($method, $args=array(), $timeout = 60){      
+        $request_data['api_key'] = $this->api_key;
+        $request_data['data'] = $args;
+        $url = $this->api_endpoint.'/'.$method;
+        
+        if (function_exists('curl_init') && function_exists('curl_setopt')){
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+            curl_setopt($ch, CURLOPT_USERAGENT, 'PHP-SUBIEKT-API/1.0');       
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, $this->verify_ssl);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($request_data));
+            $result = curl_exec($ch);
+            $this->result  = $result;
+            //var_Dump($result);
+        } else {
+            $json_data = json_encode($args);
+            $result    = file_get_contents($url, null, stream_context_create(array(
+                'http' => array(
+                    'protocol_version' => 1.1,
+                    'user_agent'       => 'PHP-SUBIEKT-API/1.0',
+                    'method'           => 'POST',
+                    'header'           => "Content-type: application/json\r\n".
+                                          "Connection: close\r\n" .
+                                          "Content-length: " . strlen($request_data) . "\r\n",
+                    'content'          => $json_data,
+                ),
+            )));
+        }
+
+        return $result ? json_decode($result, true) : false;
+    }
+
+    public function getPlainTextResult(){
+        return $this->result;
+    }
+}
+?>
